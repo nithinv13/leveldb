ba97290ce4836f047c5b03db40c6c3b5f9f1dc22
Kai Mast <kaimast@cs.wisc.edu>
2020-10-18 16:42:30 -0500

    Improved disk_stats script

--- disk_stats.py
+++ disk_stats.py
@@ -2,9 +2,10 @@
 
 from subprocess import Popen, call, DEVNULL
 
-import matplotlib
+import matplotlib.pyplot as plt
 import pandas
 import os
+import argparse
 
 # Make sure we store the db on the correct drive
 DB_PATH="./db_bench_data"
@@ -15,9 +16,22 @@ LVLDB_FNAME = "foreground_stats.csv"
 WORKLOAD = "writerandomburstsbytime"
 
 def main():
-    #run()
-    plot()
-    print("DONE")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--run", action="store_true", default=False)
+    parser.add_argument("--plot", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    if args.run:
+        run()
+
+    if args.plot:
+        plot()
+
+    if args.plot or args.run:
+        print("Done.")
+    else:
+        print("Warning: No option selected. Doing nothing.")
 
 def run():
     print("# Running Benchmark")
@@ -36,13 +50,29 @@ def plot():
     dstat = pandas.read_csv(DSTAT_FNAME, header=3)
     lvldb = pandas.read_csv(LVLDB_FNAME)
 
-    start_time = int(lvldb.time.min() / (1000*1000))
-    end_time = int(lvldb.time.max() / (1000*1000))
-    elapsed = end_time - start_time
+    lvldb.time = lvldb.time / (1000*1000)
+
+    start_time = lvldb.time.min()
+    end_time = lvldb.time.max()
+    elapsed = int(end_time - start_time)
 
     print("Ran for %i seconds (%i to %i)" % (elapsed, start_time, end_time))
 
     dstat = dstat[(dstat['epoch'] >= start_time) & (dstat['epoch'] <= end_time)]
+
+    lvldb.time = lvldb.time - start_time
+    dstat.epoch = dstat.epoch - start_time
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
+    ax.plot(dstat['epoch'], dstat['dsk/total:writ'], label="Disk writes")
+    ax.plot(dstat['epoch'], dstat['dsk/total:read'], label="Disk reads")
+
+    ax.plot(lvldb['time'], lvldb['data_written'], label='LevelDB writes')
+
+    fig.legend()
+    fig.savefig('dstat.pdf')
 
 if __name__ == "__main__":
     main()
